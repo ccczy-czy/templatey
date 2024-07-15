bf1f954901c6e6adc1ea34ba4ec2e0985d5305b7
ccczy-czy <zc2122@nyu.edu>
2024-07-16 02:00:47 +0800

    initial setups and added navbar

---
+++ app/(home)/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { SiLinkedin } from 'react-icons/si';
+import { SiGithub } from 'react-icons/si';
+import { SiInstagram } from 'react-icons/si';
+
+export default function Navbar() {
+  const socials = [
+    {
+      link: 'https://www.linkedin.com/in/ziyu-chen-q1e/',
+      label: 'Linkedin',
+      Icon: SiLinkedin,
+    },
+    {
+      link: 'https://github.com/ccczy-czy',
+      label: 'Github',
+      Icon: SiGithub,
+    },
+    {
+      link: 'https://www.instagram.com/cccc_zy/',
+      label: 'Instagram',
+      Icon: SiInstagram,
+    },
+  ];
+  return (
+    <nav className=" py-10 flex justify-between items-center">
+      <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2">
+        Ziyu Chen 🧑🏻‍💻
+      </h1>
+      <div className="flex items-center gap-5">
+        {socials.map((social, index) => {
+          return (
+            <Link key={index} href={social.link} aria-label={social.label}>
+              <social.Icon className="w-5 h-5 hover:scale-125 transition-all" />
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+}
